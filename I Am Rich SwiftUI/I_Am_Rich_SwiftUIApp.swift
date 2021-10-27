48b65dbb6774f0de7c80ce28e50aa131023d6f70
VIctor <colen.victor@icloud.com>
2021-10-27 12:02:57 -0300

    Initial Commit

---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Victor Colen on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
